002a0d3215047b65114e26d72345982080baf6c0
Jati Christanov <51022670+goodppl@users.noreply.github.com>
2026-05-12 00:04:11 +0700

    Delete TP directory

--- TP/index.js
+++
@@ -1,7 +0,0 @@
-const editorElement = document.getElementById("editor-kecil");
-const charCountElement = document.getElementById("hf");
-
-editorElement.addEventListener("input", (event) => {
-    const textLength = event.target.value.length;
-    charCountElement.textContent = textLength;
-});
